44dff1a970fe37144e627ad737f5e5b04ab54a35
Connor Lay <connor.lay@me.com>
2016-11-16 22:10:58 -0800

    Add a simple button

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,41 @@
 port module LightsOut exposing (..)
-import Html exposing (Html, text)
 
-main = text "Hello World!"
+import Html exposing (..)
+import Html.Events exposing (..)
+
+main =
+  beginnerProgram { model = model, view = view, update = update }
+
+-- MODEL
+
+type alias Model =
+  { light : Int }
+
+model : Model
+model =
+  Model 0
+
+-- UPDATE
+
+type Msg = Toggle
+
+update: Msg -> Model -> Model
+update msg model =
+  case msg of
+    Toggle ->
+      if model.light == 1 then
+         { model | light = 0 }
+      else
+         { model | light = 1 }
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div [] [
+    button [onClick Toggle] [text <| displayLight model.light]
+  ]
+
+displayLight : Int -> String
+displayLight state =
+  if state == 0 then "Off" else "On!"
