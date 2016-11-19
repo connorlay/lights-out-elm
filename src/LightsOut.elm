6626fdf13b5ac72c6d72be6c118e3d478678f21f
Connor Lay <connor.lay@me.com>
2016-11-19 15:59:51 -0800

    Add get neighbor function and test

--- src/LightsOut.elm
+++ src/LightsOut.elm
@@ -1,16 +1,20 @@
-port module LightsOut exposing (..)
+port module LightsOut exposing (Coord, model, neighbors)
 
 import Html exposing (..)
 import Html.Events exposing (..)
 import Array exposing (..)
+import Set exposing (..)
 
 main =
   beginnerProgram { model = model 4, view = view, update = update }
 
 -- MODEL
 
+type alias Grid =
+  Array (Array Bool)
+
 type alias Model =
-  { grid : Array (Array Bool) }
+  { grid : Grid }
 
 model : Int -> Model
 model n =
@@ -22,12 +26,21 @@ model n =
 
 -- UPDATE
 
-type alias Msg =
+type alias Coord =
   (Int, Int)
+
+type alias Msg =
+  Coord
 
 update : Msg -> Model -> Model
 update msg model =
   model
+
+neighbors : Coord -> Set Coord
+neighbors (r1, c1) =
+  [ (1, 0), (-1, 0), (0, 1), (0, -1) ]
+  |> Set.fromList
+  |> Set.map (\(r2, c2) -> (r1 + r2, c1 + c2))
 
 -- VIEW
 
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,29 +1,58 @@
 module Tests exposing (..)
 
-import LightsOut
+import LightsOut exposing (..)
 
 import Test exposing (..)
 import Fuzz exposing (..)
 import Expect
 
-import Array exposing (get, empty, length)
-import Maybe exposing (withDefault)
+import Array exposing (..)
+import Set exposing (..)
+import Maybe exposing (..)
+
 
 all : Test
 all =
-  describe "Creating the model"
-    [ fuzz (intRange 4 10) "Grid should always be NxN" <|
-      \n ->
-        let
-            model = LightsOut.model n
-            xLen = model
-                 |> .grid
-                 |> length
-            yLen = model
-                 |> .grid
-                 |> get 0
-                 |> withDefault empty
-                 |> length
-        in
-           Expect.true "Expected an NxN grid" <| List.all ((==) n) [xLen, yLen]
-    ]
+  describe "LightsOut" [
+
+    describe "Creating the model"
+      [ fuzz (intRange 4 10) "Grid should always be NxN" <|
+        \n ->
+          let
+              model = LightsOut.model n
+              xLen = model
+                   |> .grid
+                   |> Array.length
+              yLen = model
+                   |> .grid
+                   |> get 0
+                   |> withDefault Array.empty
+                   |> Array.length
+          in
+             Expect.true "Expected an NxN grid" <| List.all ((==) n) [xLen, yLen]
+      ],
+
+    describe "Finding neighbors of a cell"
+      [ test "Light and neighbors should toggle based on coordinates" <|
+        \() ->
+          let
+             expected = Set.fromList [ (0, 1), (1, 0), (2, 1), (1, 2) ]
+          in
+             Expect.equal expected <| LightsOut.neighbors (1, 1)
+      ],
+
+    describe "Updating the model"
+      [ test "Light and neighbors should toggle based on coordinates" <|
+        \() ->
+          let
+            expected = [
+                   [ False, True,  False, False ],
+                   [ True,  True,  True,  False ],
+                   [ False, True,  False, False ],
+                   [ False, False, False, False ] ]
+                   |> List.map (Array.fromList)
+                   |> Array.fromList
+          in
+             Expect.true "Expected 5 squares to toggle" False
+       ]
+     ]
